ab517f2570c9ad79be0ccc4884b66409f57a3619
Spoffy <4805393+Spoffy@users.noreply.github.com>
2025-10-14 23:48:30 +0100

    Initial commit

---
+++ mod.cpp
@@ -0,0 +1,13 @@
+name = "Zombies and Demons - S.O.G. Prairie Fire compatibility";
+description = "Adds zombies and demons for S.O.G. Prairie Fire units.";
+picture = "";
+actionName = "Website";
+action = "https://www.sogpf.com/";
+author = "Spoffy";
+
+
+logo = "";
+logoOver = "";
+logoSmall = "";
+
+
